15fd1b547961ddf3d5b2421515a223658b5555ef
rominavc99 <procrom1999@gmail.com>
2019-10-06 14:00:04 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoSegundoParcial/Computadora.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSegundoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para Computadora.xaml
+    /// </summary>
+    public partial class Computadora : UserControl
+    {
+        public Computadora()
+        {
+            InitializeComponent();
+            txtwarning.Visibility = Visibility.Hidden;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCodigo.Text == string.Empty || tbDescripcion.Text == string.Empty || tbDescuento.Text == string.Empty || tbPrecio.Text == string.Empty || tbProveedor.Text == string.Empty || cbMarca.Text == string.Empty)
+            {
+                txtwarning.Visibility = Visibility.Visible;
+            }
+
+            else
+            {
+                grdUserControl.Children.Clear();
+
+            }
+        }
+
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            grdUserControl.Children.Clear();
+        }
+
+        private void TbCodigo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtwarning.Visibility = Visibility.Hidden;
+        }
+
+        private void TbDescripcion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtwarning.Visibility = Visibility.Hidden;
+        }
+
+        private void TbPrecio_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtwarning.Visibility = Visibility.Hidden;
+        }
+
+        private void TbDescuento_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtwarning.Visibility = Visibility.Hidden;
+        }
+
+        private void TbProveedor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtwarning.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ ProyectoSegundoParcial/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSegundoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window  
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+        private void CbProductos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grdFormulario.Children.Clear();
+           
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (cbProductos.SelectedIndex)
+            {
+                case 0: //Celular
+                    grdFormulario.Children.Add(new Celular());
+                    
+
+                break;
+
+                case 1: //Computadora
+                    grdFormulario.Children.Add(new Computadora());
+                    
+
+                break;
+
+                case 2: //Laptop
+                    grdFormulario.Children.Add(new Laptop());
+                    
+
+                break;
+
+                case 3: //Impresora
+                    grdFormulario.Children.Add(new Impresora());
+                    
+
+                break;
+
+                case 4: //Bocina
+                    grdFormulario.Children.Add(new Bocina());
+                   ;
+                   
+
+                    break;
+
+                case 5: //Televsion
+                    grdFormulario.Children.Add(new Television());
+                   
+
+                break;
+
+                
+            }
+        }
+
+      
+       
+        
+    }
+}
